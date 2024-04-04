d6051414c5020ea9978500ef74770e130f5df869
Muhammad M <muhammadmp1997@gmail.com>
2024-04-05 00:41:09 +0330

    update the user model types

--- backend/app/models/user.go
+++ backend/app/models/user.go
@@ -1,8 +1,10 @@
 package models
 
+import "time"
+
 type User struct {
-	ID        uint   `json:"id" gorm:"primary_key"`
-	Nickname  string `json:"nickname"`
-	Bio       string `json:"bio"`
-	CreatedAt string `json:"created_at"`
+	ID        uint      `json:"id" gorm:"primary_key"`
+	Nickname  string    `json:"nickname" gorm:"type:varchar(50)"`
+	Bio       string    `json:"bio" gorm:"type:varchar(255)"`
+	CreatedAt time.Time `json:"created_at"`
 }
