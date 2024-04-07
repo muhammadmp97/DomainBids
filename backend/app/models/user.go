679fe7fffbcebce2adddca80c59d38e629a0232a
Muhammad M <muhammadmp1997@gmail.com>
2024-04-07 18:10:21 +0330

    fix the user created_at field

--- backend/app/models/user.go
+++ backend/app/models/user.go
@@ -1,10 +1,8 @@
 package models
 
-import "time"
-
 type User struct {
-	ID        uint      `json:"id" gorm:"primary_key"`
-	Nickname  string    `json:"nickname" gorm:"type:varchar(50)"`
-	Bio       string    `json:"bio" gorm:"type:varchar(255)"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        uint   `json:"id" gorm:"primary_key"`
+	Nickname  string `json:"nickname" gorm:"type:varchar(50)"`
+	Bio       string `json:"bio" gorm:"type:varchar(255)"`
+	CreatedAt string `json:"created_at" gorm:"type:timestamp"`
 }
