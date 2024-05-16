4a53a6783fc550bbdbfa4703151ebe6600bf6664
Muhammad M <muhammadmp1997@gmail.com>
2024-05-16 13:37:11 +0330

    FE: style fixed

--- frontend/src/components/Views/Home.vue
+++ frontend/src/components/Views/Home.vue
@@ -18,8 +18,8 @@ onMounted(function () {
   <div class="container lg:lg-container mx-auto pt-7">
     <section class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-y-7 gap-x-7 mt-10 mb-5">
       <auction v-for="auction of auctions" v-bind:key="auction.id" :id="auction.id"
-        :startingPrice="auction.starting_price" :domainName="auction.sld" :topLevel="auction.tld"
-        :endsAt="auction.ends_at"></auction>
+        :starting-price="auction.starting_price" :domain-name="auction.sld" :top-level="auction.tld"
+        :ends-at="auction.ends_at"></auction>
     </section>
   </div>
 </template>
