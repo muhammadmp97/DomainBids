3bb23104fc89b0f411a8fbb8b7ee61000a29effd
Muhammad M <muhammadmp1997@gmail.com>
2024-06-01 23:40:03 +0330

    FE: refactoring, prefer an attribute over multiple attributes

--- frontend/src/components/ShortAuction.vue
+++ frontend/src/components/ShortAuction.vue
@@ -3,27 +3,27 @@ import { computed } from 'vue'
 import Timer from './Timer.vue'
 import { isFuture } from '../helpers/time'
 
-const props = defineProps(['id', 'domainName', 'topLevel', 'startingPrice', 'endsAt'])
+const props = defineProps(['auction'])
 
 const startingPrice = computed(() => {
-  return new Intl.NumberFormat().format(props.startingPrice)
+  return new Intl.NumberFormat().format(props.auction.starting_price)
 })
 
-const isClosed = !isFuture(new Date(`${props.endsAt} UTC`))
+const isClosed = !isFuture(new Date(`${props.auction.ends_at} UTC`))
 </script>
 
 <template>
-  <a :href="'#/a/' + id">
+  <a :href="'#/a/' + auction.id">
     <div class="bg-gray-700 rounded shadow-md duration-300 hover:scale-105 hover:shadow-xl px-4 py-3">
       <div class="flex items-center justify-between">
         <div>
           <div :class="{ 'line-through': isClosed }" class="text-white font-bold tracking-wide'">
-            {{ domainName }}
-            <span class="text-red-500 tracking-widest pl-0.5">.{{ topLevel }}</span>
+            {{ auction.sld }}
+            <span class="text-red-500 tracking-widest pl-0.5">.{{ auction.tld }}</span>
           </div>
-          <div class="text-gray-400 text-sm mt-0.5">From ${{ startingPrice }}</div>
+          <div class="text-gray-400 text-sm mt-0.5">From ${{ auction.starting_price }}</div>
         </div>
-        <timer :time="endsAt"></timer>
+        <timer :time="auction.ends_at"></timer>
       </div>
     </div>
   </a>
--- frontend/src/components/Views/Home.vue
+++ frontend/src/components/Views/Home.vue
@@ -17,9 +17,7 @@ onMounted(function () {
 <template>
   <div class="container lg:lg-container mx-auto pt-7">
     <section class="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-y-7 gap-x-7 mt-10 mb-5">
-      <auction v-for="auction of auctions" v-bind:key="auction.id" :id="auction.id"
-        :starting-price="auction.starting_price" :domain-name="auction.sld" :top-level="auction.tld"
-        :ends-at="auction.ends_at"></auction>
+      <auction v-for="auction of auctions" v-bind:key="auction.id" :auction="auction"></auction>
     </section>
   </div>
 </template>
